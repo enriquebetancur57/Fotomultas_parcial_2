2a1d073619f847712b3b8c37bbf641c61306738a
enriq <enriquebetancur577@gmail.com>
2025-04-02 08:50:34 -0500

    Se genera la clsInfracción

---
+++ Controllers/InfraccionesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Fotomultas_parcial_2.Controllers
+{
+    public class InfraccionesController : ApiController
+    {
+
+
+
+    }
+}
